2bbf2a89deb3d015be83bc2bbf858ea3a94fdd12
Harsshad <harsshadsivsharan@gmail.com>
2024-04-28 19:07:32 +0530

    Updated code

--- WiFi-Hack-Tool.py
+++ WiFi-Hack-Tool.py
@@ -2,6 +2,7 @@ import subprocess
  
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n') 
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i] 
+print("Profile name & password")
 for i in profiles: 
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n') 
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b] 
